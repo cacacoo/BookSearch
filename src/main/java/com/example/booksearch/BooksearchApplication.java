c04350b39b7c2fb08890763435cc7840beec21c5
bart(권형신) <cacacoo@coupang.com>
2019-07-20 22:37:06 +0900

    [BOOK-000] init project

---
+++ src/main/java/com/example/booksearch/BooksearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksearchApplication.class, args);
+    }
+
+}
